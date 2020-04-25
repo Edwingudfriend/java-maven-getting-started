bddd3357c218c4fa70cedb0a81b111c1c54f51e1
Edwingudfriend <edwingudfriend@gmail.com>
2020-04-25 18:00:47 +0300

    Update RestHelloWorld.java

--- src/main/java/com/okteto/helloworld/RestHelloWorld.java
+++ src/main/java/com/okteto/helloworld/RestHelloWorld.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestHelloWorld {
-	
-	@GetMapping("/")
-	public String sayHello() {
-		return "Hello world!";
-	}
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello world from Okteto!";
+    }
 }
